97e264b7e438502c4b6f167af86a8dddc30618c2
Francesco Corti <fcorti@gmail.com>
2015-04-19 19:37:45 +0200

    Bugfix on MySql.

--- aaar-pentaho-sparkl/endpoints/kettle/src/Alfresco_4_2/MySql/AAAR_Kettle_v5.sql
+++ aaar-pentaho-sparkl/endpoints/kettle/src/Alfresco_4_2/MySql/AAAR_Kettle_v5.sql
@@ -17,6 +17,7 @@ USE `AAAR_Kettle`;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
 --
 -- Table structure for table `R_CLUSTER`
 --
--- aaar-pentaho-sparkl/endpoints/kettle/src/Alfresco_5_0/MySql/AAAR_Kettle_v5.sql
+++ aaar-pentaho-sparkl/endpoints/kettle/src/Alfresco_5_0/MySql/AAAR_Kettle_v5.sql
@@ -21,6 +21,7 @@ USE `AAAR_Kettle`;
 -- Table structure for table `R_CLUSTER`
 --
 
+
 DROP TABLE IF EXISTS `R_CLUSTER`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
