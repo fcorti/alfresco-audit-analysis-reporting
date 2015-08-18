941e50a35ae902450f42e61c5140ed7ac4ddb23a
Francesco Corti <fcorti@gmail.com>
2015-08-18 16:19:21 +0200

    Added the GetProperty webscript.

---
+++ alfresco/repository/aaar-repo-CE-v5.0.d/src/main/java/com/fcorti/aaar/AAARProperties.java
@@ -0,0 +1,22 @@
+package com.fcorti.aaar;
+
+/**
+ * A.A.A.R. properties description.
+ *
+ * @author fcorti@gmail.com
+ * @since 2.1.0
+ */
+public class AAARProperties {
+
+	// Properties.
+	public static String pentaho_protocol = "aaar.pentaho.protocol";
+	public static String pentaho_host     = "aaar.pentaho.host";
+	public static String pentaho_port     = "aaar.pentaho.port";
+	public static String pentaho_context  = "aaar.pentaho.context";
+
+	// Defaults.
+	public static String pentaho_protocol_default = "http";
+	public static String pentaho_host_default     = "localhost";
+	public static String pentaho_port_default     = "8080";
+	public static String pentaho_context_default  = "pentaho";
+}
